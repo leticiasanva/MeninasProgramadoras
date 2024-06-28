4deae8553f7dc5880cdd67ec8d395e526ef9adbb
leticiasanva <leticiadssantos14@gmail.com>
2024-06-28 17:25:49 -0300

    2024-06-28

--- primeira_semana.py
+++ primeira_semana.py
@@ -109,3 +109,87 @@ valor2 = int(input())
 resposta = valor1 - valor2
 
 print(resposta)
+
+#O aumento no salário
+valor1 = int(input())
+valor2 = int(input()) 
+
+resposta = (valor1 * valor2) / 100
+
+print(resposta)
+
+#O salário reajustado
+valor1 = int(input())
+valor2 = float(input()) 
+
+resposta1 = (valor1) * (valor2 / 100)
+resposta2 = resposta1 + valor1
+
+print(resposta2)
+
+#Minuteria
+valor1 = int(input())
+valor2 = int(input()) 
+
+resposta1 = valor1 * 60
+resposta2 = resposta1 + valor2
+
+print(resposta2)
+
+#Horas e minutos
+valor1 = int(input())
+ 
+
+resposta1 = valor1 // 60
+resposta2 = valor1 % 60
+
+print(valor1, "min = ", resposta1, "h", resposta2, "min", sep="")
+
+#Proporções
+valorEx1 = int(input())
+valorEx2 = int(input())
+valorEx3 = int(input())
+valorEx4 = int(input())
+
+valorRe1 = int(input())
+valorRe2 = int(input())
+valorRe3 = int(input())
+valorRe4 = int(input())
+
+resultado1 = (100 * valorRe1) / valorEx1
+resultado2 = (100 * valorRe2) / valorEx2
+resultado3 = (100 * valorRe3) / valorEx3
+resultado4 = (100 * valorRe4) / valorEx4
+
+print('%.1f' % resultado1)
+print('%.1f' % resultado2)
+print('%.1f' % resultado3)
+print('%.1f' % resultado4)
+
+#Problemas com juros compostos
+valor1 = float(input())
+valor2 = float(input())
+
+resposta1 = valor1 * (valor2 + 1) **1
+resposta2 = valor1 * (valor2 + 1) **2
+resposta3 = valor1 * (valor2 + 1) **3 
+
+print('%.2f' % resposta1)
+print('%.2f' % resposta2)
+print('%.2f' % resposta3)
+
+#porcentagem 1
+valor1 = float(input())
+valor2 = int(input())
+
+resposta1 = valor1 * (valor2 / 100)
+
+print('%.2f' % resposta1)
+
+#leolinda
+valor1 = int(input())
+valor2 = int(input())
+
+resposta = valor2 - valor1
+
+print(resposta)
---
+++ teste.py
@@ -0,0 +1,6 @@
+valor1 = int(input())
+valor2 = int(input())
+
+resposta = valor2 - valor1
+
+print(resposta)
